716ca8a3dcf90e7b3266b4305f4f196613b27d37
siu.ying <siu.ying@17ac7036-eb35-0410-8870-d53d5902bbe8>
2007-07-23 13:29:35 +0000

    Rename README file

--- plugins/qrcode/install.rb
+++ plugins/qrcode/install.rb
@@ -5,7 +5,6 @@ there = defined?(RAILS_ROOT) ? RAILS_ROOT : "#{here}/../../.."
 
 puts "Installing QRCode..."
 FileUtils.cp("#{here}/media/qrcode.js", "#{there}/public/javascripts/")
-FileUtils.cp("#{here}/QRCODE-README", "#{there}/")
 puts "QRCode has been installed."
 puts
-puts IO.read(File.join(File.dirname(__FILE__), 'QRCODE-README'))
+puts IO.read(File.join(File.dirname(__FILE__), 'README'))
