c43e7194436657eb61c6e72b6ff1c49151583f9a
siu.ying <siu.ying@17ac7036-eb35-0410-8870-d53d5902bbe8>
2007-07-23 16:48:51 +0000

    Updated plugin for new parameter and use Prototype instead of document.write

--- examples/simple/app/controllers/qrcode_controller.rb
+++ examples/simple/app/controllers/qrcode_controller.rb
@@ -7,5 +7,11 @@ class QrcodeController < ApplicationController
   def show
     @url = params[:url]
     @size = params[:size] || 2
+    
+    content = render_to_string :partial => 'show', :layout=> false  
+    
+    render :update do |page|
+      page.replace_html 'qrcode', content
+    end
   end
 end
