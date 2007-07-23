fc2c83e5eea86bed24027dc2530d936b5def9f9b
siu.ying <siu.ying@17ac7036-eb35-0410-8870-d53d5902bbe8>
2007-07-23 16:46:42 +0000

    Use prototype instead of document.write to generate QRCode

--- trunk/plugins/qrcode/lib/qrcode.rb
+++ trunk/plugins/qrcode/lib/qrcode.rb
@@ -1,23 +1,26 @@
 module QRCode
   module QRCodeHelper
-    def qrcode(url, size=2)
-      output = %Q{<script type="text/javascript">
+    def qrcode(url, size=2, id=nil)
+      id ||= "qrcode_#{rand()}"
+      output = %Q{<div class="qrcode" id="#{id}"></div><script type="text/javascript">
 var qr = new QRCode(4, QRErrorCorrectLevel.H);
 qr.addData('#{url}');
 qr.make();
-document.write("<table style='border-width: 0px; border-style: none; border-color: #0000ff; border-collapse: collapse;'>");
+var text = "";
+text += "<table style='border-width: 0px; border-style: none; border-color: #0000ff; border-collapse: collapse;'>";
 for (var r = 0; r < qr.getModuleCount(); r++) {
-    document.write("<tr>");
+    text += "<tr>";
     for (var c = 0; c < qr.getModuleCount(); c++) {
         if (qr.isDark(r, c) ) {
-            document.write("<td style='border-width: 0px; border-style: none; border-color: #0000ff; border-collapse: collapse; padding: 0; margin: 0; width: #{size}px; height: #{size}px; background-color: #000000;'/>");
+          text += "<td style='border-width: 0px; border-style: none; border-color: #0000ff; border-collapse: collapse; padding: 0; margin: 0; width: #{size}px; height: #{size}px; background-color: #000000;'/>";
         } else {
-            document.write("<td style='border-width: 0px; border-style: none; border-color: #0000ff; border-collapse: collapse; padding: 0; margin: 0; width: #{size}px; height: #{size}px; background-color: #ffffff;'/>");
+          text += "<td style='border-width: 0px; border-style: none; border-color: #0000ff; border-collapse: collapse; padding: 0; margin: 0; width: #{size}px; height: #{size}px; background-color: #ffffff;'/>";
         }
     }
-    document.write("</tr>");
+    text += "</tr>";
 }
-document.write("</table>");
+text += "</table>";
+Element.update($('#{id}'), text);
 </script>
       }
     end
